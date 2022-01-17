550bce4f45b5cc99779a21c878467b51988f1bac
Mylavarapu Satyanarayana <mllsatyanarayana@gmail.com>
2022-01-17 08:14:48 +0530

    Update hi.ps1

--- hi.ps1
+++ hi.ps1
@@ -1,4 +1,4 @@
 hostname
 
-$d= get-service -computername DC001.satya.com | select-object MachineName,Name,DisplayName,Status | ft -autosize
+$d= get-service -computername server1.satya.com | select-object MachineName,Name,DisplayName,Status | ft -autosize
 $d 
