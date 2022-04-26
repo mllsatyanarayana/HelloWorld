0fcf102b1ec2da027848596df54aec2b1145de34
Mylavarapu Satyanarayana <mllsatyanarayana@gmail.com>
2022-04-26 09:37:46 +0530

    Update hi.ps1

--- hi.ps1
+++ hi.ps1
@@ -2,5 +2,8 @@ hostname
 
 Write-Host "Hello World, I'm a Jenkins build!"
 
-get-service -computername ad1
+get-service -computername ad1 | select-object MachineName,Status,Name | ft -AutoSize
+
+get-service -computername server1 | select-object MachineName,Status,Name | ft -AutoSize
+
 
